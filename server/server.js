ecfdd557e04e7f6498ca593308fec9edba89315d
Shubham Kumar <shubham.kumar0109@gmail.com>
2018-03-02 18:19:06 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = express.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => {
+    console.log(`server listening at ${port}`);
+});
