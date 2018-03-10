644d06bddb711ed195642b0ec9c6ab0d4347e664
Shubham Kumar <shubham.kumar0109@gmail.com>
2018-03-10 14:56:24 +0530

    Added Scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom (){
+    var message = $('#messages');
+    var newMessage = message.children('li:last-child');
+
+    var clientHeight = message.prop('clientHeight');
+    var scrollTop = message.prop('scrollTop');
+    var scrollHeight = message.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + lastMessageHeight + newMessageHeight >= scrollHeight){
+        message.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() { 
     console.log("connected");
 });
@@ -20,6 +35,7 @@ socket.on('newMsg', function(data) {
         createdAt : time
     });
     $('#messages').append(rendered);
+    scrollToBottom();
 });
 
 $('document').ready(function(){
